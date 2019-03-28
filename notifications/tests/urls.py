902d151985c6d0d0d5fc85bf9615e7e6e4093056
belal-bh <bh.pro.pust@gmail.com>
2019-03-28 16:38:09 +0600

    fixed froblem of importing login in django-2.1

--- notifications/tests/urls.py
+++ notifications/tests/urls.py
@@ -4,21 +4,23 @@ from distutils.version import StrictVersion  # pylint: disable=no-name-in-module
 
 from django import get_version
 from django.contrib import admin
-from django.contrib.auth.views import login
+# from django.contrib.auth.views import login # login was removed in django 2.1
 from notifications.tests.views import (live_tester,  # pylint: disable=no-name-in-module,import-error
                                        make_notification)
 
 if StrictVersion(get_version()) >= StrictVersion('2.0'):
+    from django.contrib.auth.views import LoginView
     from django.urls import include, path  # noqa
     urlpatterns = [
         path('test_make/', make_notification),
         path('test/', live_tester),
-        path('login/', login, name='login'),  # reverse for django login is not working
+        path('login/', LoginView.as_view(), name='login'),  # reverse for django login is not working
         path('admin/', admin.site.urls),
         path('', include('notifications.urls', namespace='notifications')),
     ]
 else:
     from django.conf.urls import include, url
+    from django.contrib.auth.views import login
     urlpatterns = [
         url(r'^login/$', login, name='login'),  # reverse for django login is not working
         url(r'^test_make/', make_notification),
